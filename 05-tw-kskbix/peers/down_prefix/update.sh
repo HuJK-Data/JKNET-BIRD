8ba232da0b974e1cf622bcb930eb453c42b0ab61
root <root@WhoJK.kskb.moe>
2022-12-22 19:22:34 +0000

    update kskbix

--- 05-tw-kskbix/peers/down_prefix/update.sh
+++ 05-tw-kskbix/peers/down_prefix/update.sh
@@ -8,6 +8,14 @@ update_prefix () {
   else
     echo "$1 update failed"
   fi
+  if bgpq4 -tb -l asn_$1 $2 > $1.conf.tmp; then
+    echo "" >> $1.conf
+    echo -n "define " >> $1.conf
+    cat $1.conf.tmp >> $1.conf
+    rm $1.conf.tmp
+  else
+    echo "$1 update failed"
+  fi
 }
 
 update_prefix poema AS-POEMA
